9f227fb4409d0581865efb19c8e50a091d9f43fc
Vitaly Pasechnik <vitaly.pasechnik.11@gmail.com>
2023-08-09 12:55:15 +0300

    second saved

--- src/screens/ArticleSctreen.js
+++ src/screens/ArticleSctreen.js
@@ -1,21 +1,23 @@
 import React, { useContext } from 'react'
 import { Link, useParams, useNavigate } from "react-router-dom";
 import moment from 'moment';
-import { Context } from '../../src/app/context/context';
-
-import LinkIcon from '@mui/icons-material/Link';
+import { Context } from '../app/context/context';
 
 function ArticleSctreen() {
   let { id } = useParams();
   const navigate = useNavigate();
+
   const { filteredNews } = useContext(Context);
 
+
   const article = filteredNews.filter(i => i.publishedAt === id);
+
+  const formattedDate = moment().from(article.publishedAt);
 
   return (
     <div>
 
-      {/* Article Navigation */}
+      {/* Article  Navigation */}
 
       <div className="flex justify-between border border-gray-500">
 
@@ -35,7 +37,7 @@ function ArticleSctreen() {
           p-2 h-10 bg-gray-400 rounded m-3'
         >
           Home
-          <img className="w-10 m-2 bg-gray-400 rounded p-1" src="../images/logo1.png" />
+          <img className="w-10 m-2 bg-gray-400 rounded p-1" alt="article" src="../images/logo1.png" />
         </Link>
 
       </div>
@@ -45,11 +47,11 @@ function ArticleSctreen() {
       {article && (
         <div className="flex flex-col justify-center items-center p-5">
           <p className='text-xl font-bold p-3'>{article[0].title}</p>
-          <img className='p-4' src={article[0].urlToImage} alt='article image' />
+          <img className='p-4' src={article[0].urlToImage} alt='article ' />
           <p className='text-l text-white p-3'>{article[0].content}</p>
           <p className='text-l text-white p-3'>{article[0].description}</p>
           <a className='text-l text-blue-400 p-3' href={article[0].url}>Source name: {article[0].source.name}</a>
-          <p className='text-l text-white p-3'>Published: {moment().from(article[0].publishedAt)} ago</p>
+          <p className='text-l text-white p-3'>Published: {formattedDate} ago</p>
         </div>
       )}
 
